c5516618d949109e6ad6a893185f7f5129497552
joselerma97 <josemanuel@lermaserver.com>
2023-07-15 20:00:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/logo.webp">
+</head>
+
+<body>
+
+<nav>
+    <div class="nav-wrapper light-green darken-4">
+        <img src="img/logo.webp" class="brand-logo right" style="height: 55px" alt="IE Logo">
+        <div class="left">
+            SQL2 Group Assignment
+        </div>
+    </div>
+</nav>
+
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script>
+
+</script>
+</body>
+</html>
